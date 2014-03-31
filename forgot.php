ff7b1c99701342305a5cb6d0820ed0db2dff3d7c
asandow <asandow@knights.ucf.edu>
2014-03-31 16:52:09 -0400

    database updates
    updates password in database, still need to be able to confirm update

--- forgot.php
+++ forgot.php
@@ -8,51 +8,42 @@
 	//check if email is entered
 	if ($email)
 	{
-			//GET EMAIL FROM DATABASE (CHECK WITH RICHARD TO SEE HOW TO DO THIS)
-			$dbEmail = $_SESSION["email"];
+		//generate a new random password
+		$newPW = rand();
+		
+		//md5 encripts the random number password which converts it into a string
+		$newPW = md5($newPW);
+		
+		//Shorten the given string
+		$newPW = substr($newPW, 0, 10);
+						
+		//update the database with the new password
+		$CMD = sprintf("Java -jar c:\PerformQuery.jar %s %s %s", "ForgotPassword", $dbEmail, $newPW);
+		exec($CMD);
+		
+		//CHECK THE DATABASE TO MAKE SURE THE PASSWORD WAS UPDATED
+		if()
+		{
+			//email user new password
+			//Who the email is coming from
+			$webmaster = "EECS_OrderForm@knights.ucf.edu";
 			
-			//make sure the user supplied the correct corresponding email with their user name
-			if($email == $dbEmail)
+			//sets the headers and the body of the email
+			$headers = "From: $webmaster";
+			$subject = "New admin password for EECS order forms";
+			//WE NEED A CONTACT NAME AND NUMBER
+			$message = "Your new password is: $newPW. \nIf you didnt request this change, please contact ___________";
+			
+			//sends the email
+			if( mail($email, $subject, $message, $headers))
 			{
-				//generate a new random password
-				$newPW = rand();
-				
-				//md5 encripts the random number password which converts it into a string
-				$newPW = md5($newPW);
-				
-				//Shorten the given string
-				$newPW = substr($newPW, 0, 10);
-				
-				
-				//UPDATE THE DATABASE WITH THE NEW PASSWORD
-				$_POST["password"] = $newPW;
-				
-				//CHECK THE DATABASE TO MAKE SURE THE PASSWORD WAS UPDATED
-				if()
-				{
-					//email user new password
-					//Who the email is coming from
-					$webmaster = "EECS_OrderForm@knights.ucf.edu";
-					
-					//sets the headers and the body of the email
-					$headers = "From: $webmaster";
-					$subject = "New admin password for EECS order forms";
-					//WE NEED A CONTACT NAME AND NUMBER
-					$message = "Your new password is: $newPW. \nIf you didnt request this change, please contact ___________";
-					
-					//sends the email
-					if( mail($email, $subject, $message, $headers))
-					{
-						echo "Your password was reset.  Please check your email to find your new password";
-					}
-					else
-						echo "An error occured and the email was not sent. Please try again";
-				}
-				//the password was not reset
-				echo "An error occured. Your password was not reset";
+				echo "Your password was reset.  Please check your email to find your new password";
 			}
 			else
-				echo "The supplied email was incorrect";
+				echo "An error occured and the email was not sent. Please try again";
+		}
+		//the password was not reset
+		echo "An error occured. Your password was not reset";
 		
 	}
 	//alert user if email not entered
